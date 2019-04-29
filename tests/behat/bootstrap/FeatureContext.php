94221a563d6dc123581f3dc2c812a50d6bc7ba2c
chrys ugwu <chrysu@teamarmando.com>
2019-04-29 14:27:57 -0500

    Remove redundant conditional check

--- tests/behat/bootstrap/FeatureContext.php
+++ tests/behat/bootstrap/FeatureContext.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BehatContexts;
 
-
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
 use BehatContexts\contexts\Spinner;
@@ -18,16 +17,16 @@ class FeatureContext extends MinkContext implements Context
      * @param int $width
      * @param int $height
      */
-    public function iSetBrowserWindowSizeToX(int $width, int $height)
+    public function iSetBrowserWindowSizeToX($width, $height)
     {
-        $this->getSession()->resizeWindow($width, $height, 'current');
+        $this->getSession()->resizeWindow((int) $width, (int) $height, 'current');
     }
 
     /**
      * @When /^I wait for name (\d+) seconds$/
      * @param int $seconds
      */
-    public function iWaitForSeconds(int $seconds)
+    public function iWaitForSeconds($seconds)
     {
         $this->getSession()->wait($seconds * 1000);
     }
